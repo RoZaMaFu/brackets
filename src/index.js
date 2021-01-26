5c43930684cf97d283b2c0acd172e7ac27517525
RoZaMaFu <68646221+RoZaMaFu@users.noreply.github.com>
2021-01-26 13:06:55 +0300

    feat: wrote check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stringArray = str.split('')
+    if(stringArray.length % 2 !== 0){
+        return false
+    }
+    let openedConfigs = bracketsConfig.map(config => {
+        return config[0]
+    })
+    let closedConfigs = bracketsConfig.map(config => {
+        return config[1]
+    })
+    let bracketArray = []
+    let resultArray = []
+    stringArray.map((bracket, i) => {
+        if(openedConfigs.indexOf(bracket) !== -1 && openedConfigs.indexOf(stringArray[i-1]) === -1 && stringArray[i-1]){
+            resultArray.push(bracketArray)
+            bracketArray = []
+        }
+        bracketArray.push(bracket)
+        if(i === stringArray.length - 1){
+            resultArray.push(bracketArray)
+        }
+    })
+    for(let j = 0; j < resultArray.length; j++){
+        let firstPart = []
+        let secondPart = []
+        for(let i = 0; i < resultArray[j].length; i++){
+            if(i < resultArray[j].length / 2){
+                firstPart.push(resultArray[j][i])
+            }
+            else{
+                secondPart.push(resultArray[j][i])
+            }
+        }
+        for(let i = 0; i < secondPart.length; i++){
+            let index = closedConfigs.indexOf(secondPart[i])
+            secondPart[i] = openedConfigs[index]
+        }
+        secondPart = secondPart.reverse()
+        for(let i = 0; i < firstPart.length; i++){
+            if(firstPart[i] !== secondPart[i]){
+                return false
+            }
+        }
+    }
+    return true
 }
